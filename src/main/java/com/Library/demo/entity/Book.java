b7cd0b41b0b834994be572092deef575bf498652
sareddy <sallepallisiva319@gmail.com>
2022-03-14 16:57:34 +0530

    My First Commit

--- src/main/java/com/Library/demo/entity/Book.java
+++ src/main/java/com/Library/demo/entity/Book.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-// Book Entity Class
+// Book Entity Classs
 @Entity
 public class Book {
 	@Id
